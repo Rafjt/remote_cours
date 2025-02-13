ac1db03dc5956c14f87c7f291dcbf5070d343b69
Rafjt <118538315+Rafjt@users.noreply.github.com>
2025-02-13 22:32:56 +0100

    Update Boot.sh

--- linux/Boot.sh
+++ linux/Boot.sh
@@ -1,39 +1,39 @@
-#!/bin/bash
+##!/bin/bash
 
 # Installer parted
-pacman -Sy parted
+#pacman -Sy parted
 
 # À définir
-parted /dev/sda --script mklabel gpt
-parted /dev/sda --script mkpart ESP fat32 1MiB 513MiB
-parted /dev/sda --script set 1 esp on
-parted /dev/sda --script mkpart LUKS ext4 513MiB 100%
+#parted /dev/sda --script mklabel gpt
+#parted /dev/sda --script mkpart ESP fat32 1MiB 513MiB
+#parted /dev/sda --script set 1 esp on
+#parted /dev/sda --script mkpart LUKS ext4 513MiB 100%
 
 # Définition du mot de passe par défaut
-echo "azerty123" | cryptsetup luksFormat --type luks1 /dev/sda2
-echo "azerty123" | cryptsetup open /dev/sda2 cryptroot
+#echo "azerty123" | cryptsetup luksFormat --type luks1 /dev/sda2
+#echo "azerty123" | cryptsetup open /dev/sda2 cryptroot
 
 # Création des volumes LVM
-pvcreate /dev/mapper/cryptroot
-vgcreate vg0 /dev/mapper/cryptroot
-lvcreate -L 10G -n encrypted vg0   # Espace chiffré monté manuellement
-lvcreate -L 15G -n virtualbox vg0  # Espace pour VirtualBox
-lvcreate -L 5G -n shared vg0       # Dossier partagé père/fils
-lvcreate -l 100%FREE -n root vg0   # Système principal
+#pvcreate /dev/mapper/cryptroot
+#vgcreate vg0 /dev/mapper/cryptroot
+#lvcreate -L 10G -n encrypted vg0   # Espace chiffré monté manuellement
+#lvcreate -L 15G -n virtualbox vg0  # Espace pour VirtualBox
+#lvcreate -L 5G -n shared vg0       # Dossier partagé père/fils
+#lvcreate -l 100%FREE -n root vg0   # Système principal
 
 # Dernière snapshot snap3
 
 # Formatage des partitions
-ARCH=$(uname -m)  # Récupération de l'architecture
+#ARCH=$(uname -m)  # Récupération de l'architecture
 
-if [[ "$ARCH" == "x86_64" ]]; then
-    mkfs.fat -F32 /dev/sda1  # Exécuté uniquement si l'architecture est AMD64 (x86_64)
-fi
+#if [[ "$ARCH" == "x86_64" ]]; then
+#    mkfs.fat -F32 /dev/sda1  # Exécuté uniquement si l'architecture est AMD64 (x86_64)
+#fi
 
-mkfs.ext4 /dev/vg0/root
-mkfs.ext4 /dev/vg0/virtualbox
-mkfs.ext4 /dev/vg0/shared
-mkfs.ext4 /dev/vg0/encrypted
+#mkfs.ext4 /dev/vg0/root
+#mkfs.ext4 /dev/vg0/virtualbox
+#mkfs.ext4 /dev/vg0/shared
+#mkfs.ext4 /dev/vg0/encrypted
 
 
 # Configuration de la partition EFI
@@ -44,75 +44,75 @@ mkfs.ext4 /dev/vg0/encrypted
 
 
 # Montage des partitions
-mount /dev/vg0/root /mnt
-mkdir -p /mnt/boot
-mount /dev/sda1 /mnt/boot
-mkdir -p /mnt/virtualbox
-mount /dev/vg0/virtualbox /mnt/virtualbox
-mkdir -p /mnt/shared
-mount /dev/vg0/shared /mnt/shared
+#mount /dev/vg0/root /mnt
+#mkdir -p /mnt/boot
+#mount /dev/sda1 /mnt/boot
+#mkdir -p /mnt/virtualbox
+#mount /dev/vg0/virtualbox /mnt/virtualbox
+#mkdir -p /mnt/shared
+#mount /dev/vg0/shared /mnt/shared
 
 # Mise à jour des miroirs Reflector
 #reflector --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Installation de la base Arch Linux
-pacstrap /mnt base linux linux-firmware lvm2 vim sudo
+#pacstrap /mnt base linux linux-firmware lvm2 vim sudo
 
 # Génération de fstab
-genfstab -U /mnt >> /mnt/etc/fstab
+#genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot dans le système installé
-arch-chroot /mnt 
+#arch-chroot /mnt 
 
 # Configuration du fuseau horaire
-ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-hwclock --systohc
+#ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+#hwclock --systohc
 
 # Configuration de la localisation
-echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
-echo "KEYMAP=fr" > /etc/vconsole.conf
+#echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
+#echo "KEYMAP=fr" > /etc/vconsole.conf
 
 # Configuration réseau
-echo "archlinux" > /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1 localhost" >> /etc/hosts
-echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
+#echo "archlinux" > /etc/hostname
+#echo "127.0.0.1 localhost" >> /etc/hosts
+#echo "::1 localhost" >> /etc/hosts
+#echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
 
 # Installation de GRUB et configuration du bootloader
-pacman -S --noconfirm grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+#pacman -S --noconfirm grub efibootmgr
+#grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+#grub-mkconfig -o /boot/grub/grub.cfg
 
 # snapshot 7
 
-_____
+#_____
 
 # Création des utilisateurs
-useradd -m -G wheel -s /bin/bash collegue
-useradd -m -G collegue -s /bin/bash fils
-echo "collegue:azerty123" | chpasswd
-echo "fils:azerty123" | chpasswd
+#useradd -m -G wheel -s /bin/bash collegue
+#useradd -m -G collegue -s /bin/bash fils
+#echo "collegue:azerty123" | chpasswd
+#echo "fils:azerty123" | chpasswd
 
 # Activation du sudo pour le groupe wheel
-sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+#sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
 # Configuration des permissions du dossier partagé
-chown collegue:fils /shared
-chmod 770 /shared
+#chown collegue:fils /shared
+#chmod 770 /shared
 
 # Installation des paquets essentiels
-pacman -S --noconfirm xorg hyprland firefox neovim virtualbox linux-headers base-devel git htop neofetch nano
+#pacman -S --noconfirm xorg hyprland firefox neovim virtualbox linux-headers base-devel git htop neofetch nano
 
 # Activation des services essentiels
-systemctl enable NetworkManager
+#systemctl enable NetworkManager
 
 # Ajout d'un alias pour monter l'espace sécurisé
-echo 'alias mount_secure="cryptsetup open /dev/sdX2 secure && mount /dev/mapper/secure /mnt/secure"' >> /home/collegue/.bashrc
+#echo 'alias mount_secure="cryptsetup open /dev/sdX2 secure && mount /dev/mapper/secure /mnt/secure"' >> /home/collegue/.bashrc
 
-EOF
+#EOF
 
 # Fin du script
-echo "Installation terminée. Vous pouvez maintenant redémarrer."
+#echo "Installation terminée. Vous pouvez maintenant redémarrer."
 
 
 
